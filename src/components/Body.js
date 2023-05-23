5233b80c068b2d1d00c18d578a20fdd9c619010d
Naman <naman@192.168.1.3>
2023-05-24 00:52:32 +0530

    Fixed body length

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,12 +9,12 @@ import { HOME_PAGE_DESCRIPTION } from "../../constants";
 
 const Body = () => {
   return (
-    <>
+    <div className="min-h-screen">
       <ul>
         <li><Banner /></li>
-        <li><h1 className="py-10"></h1>{HOME_PAGE_DESCRIPTION}</li>
+        <li className="my-6 mx-3 py-5">{HOME_PAGE_DESCRIPTION}</li>
       </ul>
-    </>
+    </div>
   )
 
 }
